eea8b20a6921bb7a48346fade4f2828cdf03747e
Boris Rasin <boris@pointx.org>
2020-12-16 04:20:42 +0200

    set JAVA_HOME for macOS builds

--- default.sh
+++ default.sh
@@ -1,3 +1,3 @@
-export JAVA_HOME=`/usr/libexec/java_home -v 11`
+export JAVA_HOME=`/usr/libexec/java_home`
 cmake -B build/default
 cmake --build build/default
--- xcode.sh
+++ xcode.sh
@@ -1,3 +1,3 @@
-export JAVA_HOME=`/usr/libexec/java_home -v 11`
+export JAVA_HOME=`/usr/libexec/java_home`
 cmake -B build/xcode -G "Xcode"
 cmake --build build/xcode
