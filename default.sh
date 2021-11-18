a2a12e9a5bd54f9416142b9735dfa8468a223d10
Boris Rasin <boris@pointx.org>
2021-11-19 00:46:20 +0200

    remove no longer needed `export JAVA_HOME` (Mac)

--- default.sh
+++ default.sh
@@ -1,3 +1,2 @@
-export JAVA_HOME=`/usr/libexec/java_home`
 cmake -B build/default
 cmake --build build/default
--- xcode.sh
+++ xcode.sh
@@ -1,3 +1,2 @@
-export JAVA_HOME=`/usr/libexec/java_home`
 cmake -B build/xcode -G "Xcode"
 cmake --build build/xcode
